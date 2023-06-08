95f3c9ab94e0f94761fcf23cbf239fb3593b3e91
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-06-08 16:25:56 -0400

    feat: load colours and font

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,12 @@
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='h-[100dvh] w-full grid place-items-center'>
+      <div>
+        <h1 className='text-6xl text-red font-black'>TED<sup>x</sup></h1>
+        <h1 className='text-6xl font-light'>ColumbiaLake</h1>
+        <h1 className='text-6xl font-bold'>Youth</h1>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#eb0027",
+        black: "#000000"
+      }
+    },
   },
   plugins: [],
 }
