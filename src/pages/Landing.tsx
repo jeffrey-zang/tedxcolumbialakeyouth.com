459d0dfe9a4b865448153d765b3a1d1bf2cf1ada
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-06-09 22:01:25 -0400

    feat: gradient animation

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -22,14 +22,14 @@ const Landing = () => {
       <div className='landing bg-gray-50 text-gray-950'>
         <div>
           <h2>TED<sup>x</sup>Columbia Lake Youth</h2>
-          <h1>Let's reimagine <br/><span className='gradient-text bg-gradient-to-r from-orange-300 to-red'>innovation</span></h1>
+          <h1>Let's reimagine <br/><span className='gradient-text bg-gradient-to-r from-orange-300 via-red to-orange-300'>innovation</span></h1>
           <h3>October 7 • Kitchener Public Library</h3>
         </div>
       </div>
       <div ref={ref} className='landing bg-gray-950 text-gray-50'>
         <div>
           <h2>TED<sup>x</sup>Columbia Lake Youth</h2>
-          <h1>Let's reenvision <br/><span className='gradient-text bg-gradient-to-r from-red to-purple-400'>education</span></h1>
+          <h1>Let's reenvision <br/><span className='gradient-text bg-gradient-to-r from-red via-purple-400 to-red'>education</span></h1>
           <h3>October 7 • Kitchener Public Library</h3>
         </div>
       </div>
