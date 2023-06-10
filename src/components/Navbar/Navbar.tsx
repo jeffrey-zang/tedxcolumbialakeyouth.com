8d234c172835a048505413dedcfddffafc4bb029
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-06-10 17:51:00 -0400

    fix(landing): responsive font sizes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,11 +3,11 @@ import './Navbar.scss'
 const Navbar = () => {
   return (
     <div className='z-20 py-6 px-[10vw] fixed w-full top-0 bg-gray-950 navbar'>
-      <div className='flex justify-center md:justify-between items-center text-xl'>
+      <div className='flex justify-center lg:justify-between items-center text-xl'>
         <h2 className='text-white font-bold'>
           <span className='font-black text-red'>TED<sup>x</sup></span><span className='font-normal'>Columbia Lake</span> Youth
         </h2>
-        <div className='links hidden md:flex items-center'>
+        <div className='links hidden lg:flex items-center'>
           <p>Speakers</p>
           <p>About</p>
           <p>Our Team</p>
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -21,14 +21,14 @@ const Landing = () => {
     <div onMouseMove={(e) => handleMove(e)} onTouchMove={(e) => handleMove(e.touches[0])}>
       <img src={wave} alt='wave' className='absolute bottom-0 w-full z-40' />
       <div className='landing bg-gray-100 right text-gray-900 h-full'>
-        <div className='h-full flex flex-col justify-center'>
+        <div className='h-full flex flex-col md:gap-0 gap-4 justify-center'>
           <h2><span className='font-black text-red'>TED<sup>x</sup></span>Columbia Lake <span className='font-bold'>Youth</span></h2>
           <h1>Let's <br className='block md:hidden'/>reimagine <br/><span className='gradient-text bg-gradient-to-r from-orange-300 via-red to-orange-300'>empowerment</span></h1>
           <h3>October 7, 2023 • Kitchener Public Library Theatre • Tickets available soon</h3>
         </div>
       </div>
       <div ref={ref} className='landing bg-gray-900 left text-gray-100 h-full'>
-        <div className='h-full flex flex-col justify-center'>
+        <div className='h-full flex flex-col md:gap-0 gap-4 justify-center'>
           <h2><span className='font-black text-red'>TED<sup>x</sup></span>Columbia Lake <span className='font-bold'>Youth</span></h2>
           <h1>Let's <br className='block md:hidden'/>reinvent <br/><span className='gradient-text bg-gradient-to-r from-red via-purple-400 to-red'>education</span></h1>
           <h3>October 7, 2023 • Kitchener Public Library Theatre • Tickets available soon</h3>
