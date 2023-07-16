b554bd4ee82f733d2344c1bd2ac4695dc57e3616
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-07-15 20:33:48 -0400

    refac(deps): switch to react icons

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -1,8 +1,10 @@
+import { BsInstagram } from 'react-icons/bs'
+
 const Socials = () => {
   return (
     <div className='flex gap-2'>
       <div onClick={() => window.open('https://www.instagram.com/tedxcolumbialakeyouth/')}>
-        <i className="bi bi-instagram text-white hover:text-red cursor-pointer transition-colors"></i>
+        <BsInstagram className="text-white hover:text-red cursor-pointer transition-colors" />
       </div>
       {/* <div>
         <i className="bi bi-linkedin text-white hover:text-red cursor-pointer transition-colors"></i>
