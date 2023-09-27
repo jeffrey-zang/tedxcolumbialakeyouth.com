15fb48ff7df723e81bd997fceb1b67381f611de8
Jeffrey Zang <jeffrey.zang.07@gmail.com>
2023-09-27 14:26:21 -0400

    chore: update sponsors

--- src/data/Partners.data.tsx
+++ src/data/Partners.data.tsx
@@ -1,14 +1,63 @@
 import { Partner } from "../types/partners"
 import ycf from '../assets/partners/ycf.png'
 import ked from '../assets/partners/ked.jpg'
+import kit from '../assets/partners/kit.jpg'
+import swril from '../assets/partners/swril.jpg'
+import meal from '../assets/partners/meal.png'
+import ctrlv from '../assets/partners/ctrlv.png'
+import fog from '../assets/partners/fog.png'
+import sweet from '../assets/partners/sweet.avif'
+import pillers from '../assets/partners/Pillers.webp'
+import sd from '../assets/partners/sd.png'
+import idle from '../assets/partners/idle.webp'
+import sjp from '../assets/partners/SJP.png'
+import wrsbc from '../assets/partners/wrsbc.png'
+import wrdsb from '../assets/partners/wrdsb.png'
 
 export const partnerData = [
   {
-    "logo": ycf,
-    "href": "https://youthcreativityfund.ca",
-    "class": "bg-black border-2 border-white"
+    logo: ycf,
+    href: "https://youthcreativityfund.ca",
+    class: "bg-black border-2 border-white"
   }, {
-    "logo": ked,
-    "href": "https://www.tedxkitchenered.com",
+    logo: ked,
+    href: "https://www.tedxkitchenered.com",
+  }, {
+    logo: swril,
+    href: "https://www.swril.ca"
+  }, {
+    logo: meal,
+    href: "https://www.mealinajar.com"
+  }, {
+    logo: sweet,
+    href: "https://sweetlouscookies.com"
+  }, {
+    logo: kit, 
+    href: "https://www.kitchener.ca/en/index.aspx"
+  }, {
+    logo: ctrlv,
+    href: "https://www.ctrlv.ca"
+  }, {
+    logo: fog,
+    href: "https://www.fogoffclothing.com",
+    class: "bg-[#000] border-2 border-white"
+  }, {
+    logo: pillers,
+    href: "https://www.pillers.com"
+  }, {
+    logo: sd,
+    href: "https://sweetdreamsteashop.com"
+  }, {
+    logo: idle,
+    href: "https://idleandwood.ca"
+  }, {
+    logo: sjp, 
+    href: "http://www.stjacobsprintery.com"
+  }, {
+    logo: wrsbc,
+    href: "https://www.waterlooregionsmallbusiness.com"
+  }, {
+    logo: wrdsb,
+    href: "https://www.wrdsb.ca"
   }
 ] as Partner[]
--- src/sections/Partners/index.tsx
+++ src/sections/Partners/index.tsx
@@ -29,7 +29,7 @@ const Partners = () => {
     <div className='typography mt-16 flex-col mx-auto scroll-m-28' id='partners'>
       <h2 className='text-center'>Our Partners</h2>
       <LazyLoadImage key='-1' src={uw} alt='sponsor' onClick={() => window.open('https://uwaterloo.ca')} effect='blur' wrapperClassName='partner-logo p-4 !w-1/2 mx-auto !grid place-items-center' />
-      <div className='flex flex-col md:flex-row justify-center items-center gap-4 mt-8'>
+      <div className='flex justify-center flex-wrap gap-4 mt-8'>
         {partnerData.map((partner, index) => {
           return (
             <LazyLoadImage key={index} effect='blur' wrapperClassName='partner-logo' className={`partner-logo p-4 ${partner.class}`} src={partner.logo} alt='sponsor' onClick={() => window.open(partner.href)} />
