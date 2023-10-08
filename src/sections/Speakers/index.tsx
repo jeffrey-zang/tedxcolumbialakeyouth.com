4359566b8cfb28cd2d46e4b1c132bb7ab9120891
Jeffrey Zang <jeffrey.zang.07@gmail.com>
2023-10-08 15:05:22 -0400

    chore: add MC craig norris

--- src/sections/Speakers/index.tsx
+++ src/sections/Speakers/index.tsx
@@ -2,11 +2,24 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { speakers, keynotes } from '../../data/Speakers.data'
 import './Speakers.scss'
+import norris from '../../assets/speakers/cnorris.jpg'
 
 const Speakers = () => {
   return (
     <div className='flex flex-col items-center pt-24' id='speakers'>
-      <h2>Keynotes</h2>
+      <h2>Master of Ceremonies</h2>
+      <LazyLoadImage
+        src={
+          norris
+        }
+        alt='Craig Norris'
+        effect='blur' 
+        wrapperClassName='md:w-1/4 rounded-full aspect-square'
+        className="rounded-full aspect-square object-cover border-2 border-white mt-4"
+      />
+      <h3 className='mt-4 text-2xl'>Craig Norris</h3>
+      <p>Host, CBC K-W’s The Morning Edition on 89.1 FM and CBC Listen</p>
+      <h2 className='mt-16'>Keynotes</h2>
       <div className="flex justify-center flex-wrap gap-12 mt-4 px-20">
         {Object.entries(keynotes).map((keynote:any) => {
           keynote = keynote[1]
